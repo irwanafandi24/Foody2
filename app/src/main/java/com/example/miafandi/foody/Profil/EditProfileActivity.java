f272975b50baf0d84c4d876e5e933e042d20fb74
naofalhakim <hakimnaofal@gmail.com>
2018-02-09 14:40:04 +0700

    nambah transaksi

--- app/src/main/java/com/example/miafandi/foody/Profil/EditProfileActivity.java
+++ app/src/main/java/com/example/miafandi/foody/Profil/EditProfileActivity.java
@@ -2,11 +2,15 @@ package com.example.miafandi.foody.Profil;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.miafandi.foody.R;
 
 public class EditProfileActivity extends AppCompatActivity {
 
+    private Button btnSimpan;
+    private TextView txtNama, txtLokasi, txttanggal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
